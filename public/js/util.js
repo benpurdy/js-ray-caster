d55c11b247a188269881db4dfce24b1e79892f9d
Ben Purdy <ben.purdy@gmail.com>
2015-06-11 23:05:44 -0700

    removed old distance function

--- public/js/util.js
+++ public/js/util.js
@@ -31,7 +31,3 @@ function distance(x1,y1, x2,y2){
 	stats.counters.distance++;
 	return (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1);
 }
-
-function distanceh(x1,y1, x2,y2){
-	return Math.sqrt( (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) );
-}
